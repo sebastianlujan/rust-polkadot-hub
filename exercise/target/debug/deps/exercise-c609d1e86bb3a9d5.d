66577847cd14f72751dc6b82db6fcd036049908d
sebastianlujan <glujan.recalde@gmail.com>
2023-04-22 17:14:02 -0300

    test

---
+++ exercise/target/debug/deps/exercise-c609d1e86bb3a9d5.d
@@ -0,0 +1,5 @@
+/Users/SFProg/Documents/code/coding-rust/rust-polkadot-hub/exercise/target/debug/deps/exercise-c609d1e86bb3a9d5.rmeta: src/main.rs
+
+/Users/SFProg/Documents/code/coding-rust/rust-polkadot-hub/exercise/target/debug/deps/exercise-c609d1e86bb3a9d5.d: src/main.rs
+
+src/main.rs:
---
+++ exercise/target/debug/deps/exercise-e643a568f10fb1ef.d
@@ -0,0 +1,5 @@
+/Users/SFProg/Documents/code/coding-rust/rust-polkadot-hub/exercise/target/debug/deps/exercise-e643a568f10fb1ef.rmeta: src/main.rs
+
+/Users/SFProg/Documents/code/coding-rust/rust-polkadot-hub/exercise/target/debug/deps/exercise-e643a568f10fb1ef.d: src/main.rs
+
+src/main.rs:
